c777fae8c6c695d5777a59a42d21203b1d47cb9c
Douglas Augusto <douglasaugusto1103@gmail.com>
2023-03-30 15:59:12 -0300

    Scroll-behavior added

--- src/globalStyles/index.js
+++ src/globalStyles/index.js
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         font-family: 'Montserrat', sans-serif;
+        scroll-behavior: smooth;
     }
     a {
         text-decoration: none;
